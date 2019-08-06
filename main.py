e98b5b3e1f00de70961ad59078fa5fe28c828ac3
Ubuntu <ubuntu@ip-172-31-46-30.ec2.internal>
2019-08-06 17:19:49 +0000

    Starter code

---
+++ main.py
@@ -0,0 +1,17 @@
+import webapp2
+import os
+import jinja2
+
+jinja_current_directory = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class home(webapp2.RequestHandler):
+    def get(self):
+        start_template=jinja_current_directory.get_template("templates/homepage.html")
+        self.response.write(start_template.render())
+
+app = webapp2.WSGIApplication([
+    ('/', home)
+], debug=True)
